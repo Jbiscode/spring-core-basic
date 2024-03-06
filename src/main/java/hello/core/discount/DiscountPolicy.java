baae3617cd9b071612f961a4e65c65ba3b2d9c27
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-03-06 16:51:10 +0900

    Feature : Discount 도메인 개발

--- src/main/java/hello/core/discount/DiscountPolicy.java
+++ src/main/java/hello/core/discount/DiscountPolicy.java
@@ -1,2 +1,19 @@
-package hello.core.discount;public interface DiscountPolicy {
+package hello.core.discount;
+
+import hello.core.member.Member;
+
+/**
+ * 이 인터페이스는 할인 정책을 나타냅니다.
+ * 이 인터페이스의 구현체는 자체 할인 규칙을 정의할 수 있습니다.
+ */
+public interface DiscountPolicy {
+
+    /**
+     * 멤버에 대한 가격에 할인을 적용합니다.
+     *
+     * @param member 할인이 적용될 멤버.
+     * @param price 할인 전 원래 가격.
+     * @return 적용될 할인 금액.
+     */
+    int discount(Member member, int price);
 }
--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -1,2 +1,29 @@
-package hello.core.discount;public class FixDiscountPolicy {
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+/**
+ * 고정 할인 정책을 구현하는 클래스입니다.
+ * VIP 등급의 멤버에게만 할인이 적용됩니다.
+ */
+public class FixDiscountPolicy implements DiscountPolicy {
+    private int discountFixAmount = 1000; // 1000원 할인
+
+    /**
+     * 멤버에 대한 가격에 할인을 적용합니다.
+     * 멤버가 VIP 등급인 경우에만 할인이 적용됩니다.
+     *
+     * @param member 할인이 적용될 멤버.
+     * @param price 할인 전 원래 가격.
+     * @return 적용될 할인 금액. VIP 등급이 아닌 경우 0을 반환합니다.
+     */
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return discountFixAmount;
+        } else{
+            return 0;
+        }
+    }
 }
