8eb5c06ee8e2c95e9a36bed936d46f9bf756a39f
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-03-06 16:51:37 +0900

    Feature : Order 도메인 개발

--- src/main/java/hello/core/order/Order.java
+++ src/main/java/hello/core/order/Order.java
@@ -1,2 +1,28 @@
-package hello.core.order;public class Order {
+package hello.core.order;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter@Setter
+public class Order {
+    private Long memberId;
+    private String itemName;
+    private int itemPrice;
+    private int discountPrice;
+
+    public int calculatePrice() {
+        return itemPrice - discountPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "memberId=" + memberId +
+                ", itemName='" + itemName + '\'' +
+                ", itemPrice=" + itemPrice +
+                ", discountPrice=" + discountPrice +
+                '}';
+    }
 }
